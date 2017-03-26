dec100fc2140636156996ce888f3ba65512eac07
Irving <irving.java@gmail.com>
2017-03-26 00:53:28 -0600

    ModelResponse

---
+++ app/src/main/java/com/dev/celulaspaz/model/Recompensa.java
@@ -0,0 +1,81 @@
+package com.dev.celulaspaz.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Irving on 25/03/2017.
+ */
+
+public class Recompensa
+{
+
+    @SerializedName("_id")
+    @Expose
+    private String id;
+    @SerializedName("__v")
+    @Expose
+    private int v;
+    @SerializedName("promocion")
+    @Expose
+    private String promocion;
+    @SerializedName("imagen")
+    @Expose
+    private String imagen;
+    @SerializedName("codigo")
+    @Expose
+    private String codigo;
+    @SerializedName("fecha")
+    @Expose
+    private String fecha;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getV() {
+        return v;
+    }
+
+    public void setV(int v) {
+        this.v = v;
+    }
+
+    public String getPromocion() {
+        return promocion;
+    }
+
+    public void setPromocion(String promocion) {
+        this.promocion = promocion;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+
+}
