180fca3b6d4eb6dca1366e77b5d56a02a026f7f4
Irving <irving.java@gmail.com>
2017-03-26 00:53:11 -0600

    Interface for Click

---
+++ app/src/main/java/com/dev/celulaspaz/util/OnClickRecompensa.java
@@ -0,0 +1,12 @@
+package com.dev.celulaspaz.util;
+
+import com.dev.celulaspaz.model.Recompensa;
+
+/**
+ * Created by Irving on 26/03/2017.
+ */
+
+public interface OnClickRecompensa
+{
+    void onItemClick(Recompensa recompensa);
+}
